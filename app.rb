9a97d7a833f1c88cf197d3f7925b24dcc296aa20
john <john.d.leonetti@gmail.com>
2021-04-10 14:58:33 -0700

    commit tree dfacb7de2e48a92d409f0fda5d4a8ce75ec3c031

--- app.rb
+++ app.rb
@@ -4,6 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      
+      erb :root
+    end
+    
+    get '/new' do
+      erb :new
+    end
+    post '/pirates' do
+      
+      @new_pirates = Pirate.new(params[:pirate])
 
+      params[:pirate][:ships].each do |ship|
+        new_ship = Ship.new(ship)
+
+        Ship.all
+      end
+      @ships = Ship.all
+      erb :pirates
+    end
   end
 end
